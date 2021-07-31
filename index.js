4268de7a649ff7cdb86cccd80e83af35d49579d6
Samuel Reeder <samuel.reeder8@gmail.com>
2021-07-31 00:38:16 -0600

    Joining, help, info

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const Times = sequelize.define('times', {
     name: Sequelize.STRING,
     time: Sequelize.DATE,
     username: Sequelize.STRING,
+    participants: Sequelize.STRING,
     usage_count: {
         type: Sequelize.INTEGER,
         defaultValue: 0,
@@ -29,11 +30,9 @@ const Times = sequelize.define('times', {
     },
 });
 
-
-
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.time}!`);
-    Times.sync({ force: true });
+    Times.sync();
 });
 
 client.on('message', async message => {
@@ -43,6 +42,7 @@ client.on('message', async message => {
         const commandArgs = input.join(' ');
 
         const timeList = await Times.findAll();
+        const d = new Date();
         timeList.forEach(i => {
             if (i.time.getTime() < d.getTime()) {
                 console.log("worked");
@@ -54,14 +54,17 @@ client.on('message', async message => {
             const splitArgs = commandArgs.split(' ');
 
             const timeName = splitArgs.shift();
-            const timeValue = splitArgs.join(' ');
+            const timeValue = parseTime(splitArgs.join(' '));
 
+            if (timeValue.getTime() < d.getTime()) {
+                return message.reply(`Ummm ... that time has already past`);
+            }
             try {
                 const timeList = await Times.findAll();
                 const time = await Times.create({
                     id: timeList.length === 0 ? 0 : timeList[timeList.length - 1].id + 1,
                     name: timeName,
-                    time: parseTime(timeValue),
+                    time: timeValue,
                     username: message.author.username,
                 });
                 return message.reply(`Scheduled ${time.name} for ${time.time}`);
@@ -73,12 +76,14 @@ client.on('message', async message => {
                 console.log(e);
                 return message.reply('Something went wrong');
             }
-        } else if (command === 'time') {
+        } else if (command === 'join') {
             const timeName = commandArgs;
-            const time = await Times.findOne({ where: { name: timeName } });
+            const draft = await Times.findOne({ where: { name: timeName } });
+
+            const time = await Times.update({ participants: draft.participants += " | " + message.author.username }, { where: { name: timeName } });
+
             if (time) {
-                time.increment('usage_count');
-                return message.channel.send(time.get('time'));
+                return message.reply(" has joined");
             }
             return message.reply(`Could not find time: ${timeName}`);
         } else if (command === 'edittime') {
@@ -96,7 +101,20 @@ client.on('message', async message => {
 
             const time = await Times.findOne({ where: { name: timeName } });
             if (time) {
-                return message.channel.send(`${timeName} was created by ${time.username} at ${time.createdAt} and has been used ${time.usage_count} times.`);
+                var infoEmbed = {
+                    color: 0x0099ff,
+                    title: `${time.name} info`,
+                    fields: [],
+                    timestamp: new Date(),
+                };
+                const arr = time.participants.split(" | ");
+                arr.forEach(t => {
+                    console.log(t);
+                    if (t != null && t != "" && t != "null") {
+                        infoEmbed.fields.push({ name: t, value: "Joined", inline: true });
+                    }
+                });
+                return message.channel.send({ embed: infoEmbed });
             }
             return message.reply(`Could not find time: ${timeName}`);
         } else if (command === 'times') {
@@ -117,7 +135,7 @@ client.on('message', async message => {
                     inline: true
                 })
             });
-            
+
             return message.channel.send({ embed: scheduleEmbed });
         } else if (command === 'removetime') {
             const timeName = commandArgs;
@@ -125,6 +143,38 @@ client.on('message', async message => {
             if (!rowCount) return message.reply('That time did not exist.');
 
             return message.reply('Tag deleted.');
+        } else if (command === 'helpme') {
+            const embed = new Discord.MessageEmbed()
+                .setTitle("Help")
+                .setColor(0x0099ff)
+                .setDescription("Some stuff")
+                .setFooter("View my github repo for more", "https://github.com/SamuelReeder/discord-scheduler")
+                // .setImage("")
+                // .setThumbnail("")
+                .setTimestamp()
+                // .setURL("")
+                .addFields({
+                    name: `${PREFIX}addtime`,
+                    value: "Add an event time \nparams: {name} {time}"
+                })
+                .addFields({
+                    name: `${PREFIX}times`,
+                    value: "Displays all upcoming event times"
+                })
+                .addFields({
+                    name: `${PREFIX}removetime`,
+                    value: "Removes a specified event \nparams: {name}"
+                })
+                .addFields({
+                    name: `${PREFIX}timeinfo`,
+                    value: "Shows info about an event \nparams: {name}"
+                })
+                .addFields({
+                    name: `${PREFIX}join`,
+                    value: "Joins an event \nparams: {name}"
+                })
+
+            message.channel.send(embed);
         }
     }
 
